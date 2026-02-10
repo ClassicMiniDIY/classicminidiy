86446505d9ae44bd475855baaf4a3800d90ca9d1
Cole Gentry <peapod2007@gmail.com>
2026-02-10 09:07:52 -0500

    more posthog fixes
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- server/routes/t/static/[...path].ts
+++ server/routes/t/static/[...path].ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
   const path = getRouterParam(event, 'path') || '';
-  const target = `https://us.i.posthog.com/${path}`;
+  const target = `https://us-assets.i.posthog.com/static/${path}`;
   return proxyRequest(event, target);
 });
